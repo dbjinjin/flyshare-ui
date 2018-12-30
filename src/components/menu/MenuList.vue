318d694ef775169979b0aa71d12f00b9cb982776
DESKTOP-JOVI9FA\wuxiaohai <dbjinjin0820@qq.com>
2018-12-30 22:25:16 +0800

    no message

---
+++ src/components/menu/MenuList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{message}}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "MenuList",
+    data(){
+      return {
+        "message":"菜单列表"
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import MenuList from '@/components/menu/MenuList'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    }, {
+      path: '/menu-list',
+      name: 'MenuList',
+      component: MenuList
     }
   ]
 })
